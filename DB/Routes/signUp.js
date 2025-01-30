50165e918f9df85c2380d1b1132b3128c036fcca
Keemo Kimo <thebunthong@gmail.com>
2025-01-30 12:38:57 -0400

    Error Handling with Registration
    Successfully implemented methods to handle error when user are trying to register an account that already exist.

--- DB/Routes/signUp.js
+++ DB/Routes/signUp.js
@@ -3,15 +3,25 @@ const router = express.Router();
 const pool = require('../pool');
 
 router.post('/signup', (req, res) => {
+    const searchQuery = `SELECT * FROM users WHERE username = '${req.body.fUsername_Register}'
+    OR emailaddress = '${req.body.fEmail_Register}'`;
+
     const query = `INSERT INTO users (username, emailaddress, password) 
     VALUES ('${req.body.fUsername_Register}' , '${req.body.fEmail}' , '${req.body.fPassword_Register}')`;
 
-    pool.query(query, (error, result) => {
-        if(error){
-            console.error('Error occured : ', error);
-            res.status(500).send('An error occured while adding user to the database');
+    pool.query(searchQuery, (error, result) => {
+        if (result.rowCount > 0) {
+            return res.status(500).json({ success: false, message: 'User Already Exists!!' });
         } else {
-            res.redirect('/');
+            pool.query(query, (error, result) => {
+                if (error) {
+                    console.error('Error occured : ', error);
+                    res.status(500).send('An error occured while adding user to the database');
+                } else {
+                    console.log("Account Registered succesfully!");
+                    res.redirect('/');
+                }
+            });
         }
     });
 });
