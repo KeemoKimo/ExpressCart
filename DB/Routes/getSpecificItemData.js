1ad361fd4d5649a3756425d6784119280c6bf260
Keemo Kimo <thebunthong@gmail.com>
2025-02-26 12:17:43 -0400

    Identify if the logged in user is the owner of viewed item
    Differenciate between if logged in user and the owner of the item is the same.

--- DB/Routes/getSpecificItemData.js
+++ DB/Routes/getSpecificItemData.js
@@ -3,24 +3,59 @@ const router = express.Router();
 const pool = require('../pool');
 
 router.get('/item', (req, res) => {
-    const itemID = req.query.itemID; 
-    console.log(itemID);   
+    const itemID = req.query.itemID;
+    const userName = req.query.loggedInUser;
+    console.log(itemID);
+    console.log(userName);
 
-    let query = `SELECT * FROM items join itemimages ON items.id = itemimages.itemid WHERE items.id = $1`;
+    let userIdQuery = `SELECT id from users WHERE username = $1`;
 
-    console.log("NOT it here");
-    pool.query(query, [itemID], (error, result) => {
-        if(error){
-            console.error('Error occurred:', error);
-            res.status(500).json({ error: 'Database error' });
-        }else{
-            if (result.rowCount === 0) {
-                res.status(404).json({ error: 'Item not found' });
-            } else {
-                console.log("MAde to fetch data!");
-                res.json(result.rows[0]);
-            }
+    pool.query(userIdQuery, [userName], (error, userResult) => {
+        if (error) {
+            console.error('Error occurred while getting user:', error);
+            res.status(500).json({ error: 'Database error while getting user' });
+            return;
         }
+
+        if (userResult.rowCount === 0) {
+            res.status(404).json({ error: 'User not found' });
+            return;
+        }
+
+        const loggedInID = userResult.rows[0].id;
+        console.log('Logged in user:', loggedInID);
+
+        let query = `SELECT items.id AS item_id, 
+            itemimages.id AS image_id, 
+            items.ownerid AS owner_id,
+            items.*, 
+            itemimages.*
+            FROM items 
+            JOIN itemimages ON items.id = itemimages.itemid
+            WHERE items.id = $1`;
+
+        pool.query(query, [itemID], (error, result) => {
+            if (error) {
+                console.error('Error occurred:', error);
+                res.status(500).json({ error: 'Database error' });
+            } else {
+                if (result.rowCount === 0) {
+                    res.status(404).json({ error: 'Item not found' });
+                } else {
+                    const data = result.rows[0];
+
+                    data.loggedInUser = loggedInID;
+
+                    delete data.itemid;
+                    delete data.ownerid;
+                    delete data.id;
+
+                    console.log(result.rows[0]);
+                    res.json(result.rows[0]);
+                }
+            }
+        });
+
     });
 });
 
--- JS/specificItem.js
+++ JS/specificItem.js
@@ -1,31 +1,35 @@
 document.addEventListener("DOMContentLoaded", function () {
     const itemID = new URLSearchParams(window.location.search).get("itemId");
+    const loggedInUser = new URLSearchParams(window.location.search).get("loggedInUser");
 
     function loadItemData() {
-        fetch(`/api/item?itemID=${itemID}`)
+        fetch(`/api/item?itemID=${itemID}&loggedInUser=${loggedInUser}`)
             .then(response => response.json())
             .then(data => {
 
-                console.log(data);
+                if (data.loggedInUser === data.owner_id) {
+                    const lblHello = document.getElementById("lblHello");
+                    lblHello.innerText = "You are owner of this item!";
+                }
 
                 const itemImage = document.getElementById("itemImage");
                 itemImage.src = data.url || 'https://res.cloudinary.com/dkgfcemw4/image/upload/v1739280006/no-photo-or-blank-image-icon.jpg';
 
                 const itemId = document.getElementById("lblItemId");
-                itemId.innerText = data.id;
-                
+                itemId.innerText = data.itemid;
+
                 const itemTitle = document.getElementById("lblItemTitle");
                 itemTitle.innerText = data.name;
-                
+
                 const itemLocation = document.getElementById("lblLocation");
                 itemLocation.innerText = data.location;
-                
+
                 const itemDatePosted = document.getElementById("lblDatePosted");
                 itemDatePosted.innerText = data.dateposted;
-                
+
                 const itemPrice = document.getElementById("lblItemPrice");
                 itemPrice.innerText = data.price;
-                
+
                 const itemCondition = document.getElementById("lblItemCondition");
                 itemCondition.innerText = data.condition;
 
