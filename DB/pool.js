d23bd90b31e71c37e9f299128e8158221b0589a4
Keemo Kimo <thebunthong@gmail.com>
2025-03-17 09:08:35 -0300

    env
    env

--- DB/pool.js
+++ DB/pool.js
@@ -1,11 +1,12 @@
+require('dotenv').config();
 const { Pool } = require('pg');
 
 const pool = new Pool({
-    host: 'localhost',
-    user: 'postgres',
-    database: 'ExpressCart',
-    password: '29102006$Va',
-    port: 5432,
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
 });
 
 module.exports = pool;
